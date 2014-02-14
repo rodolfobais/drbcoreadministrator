f3fba74be5deb6bae3d19cf2709519884e3f790d
rodolfobais <rodolfo282@gmail.com>
2014-02-14 10:39:52 -0300

    Primer commit

---
+++ modules/cargarportada/upload.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+$allowed = array('png', 'jpg', 'gif','zip');
+
+if(isset($_FILES['imagen']) && $_FILES['imagen']['error'] == 0){
+	//echo "TRUE";
+	$extension = pathinfo($_FILES['imagen']['name'], PATHINFO_EXTENSION);
+// 	echo "ext: ".$extension;
+	$nombre = $id.".".$extension;
+	if(!in_array(strtolower($extension), $allowed)){
+		echo '{"status":"error"}';
+		die;
+	}
+// 	echo "fn: ".move_uploaded_file($_FILES['imagen']['tmp_name'], 'uploads/'.$_FILES['imagen']['name']);
+	if(move_uploaded_file($_FILES['imagen']['tmp_name'], '../../../gallery/'.$_FILES['imagen']['name'])){
+		header('Location: /mabmakeup/admin/index.php?menu=cargarportada');
+		echo '{"status":"success"}';
+		die;
+	}else{		
+		echo '{"status":"ErrorMoverTmp"}';
+		die;
+	}
+}else{
+	//echo "FALSE";
+	echo '{"status":"errorGral", "err":"'.$_FILES['imagen']['error'].'", "name":"'.$_FILES['imagen']['name'].'"}';
+	die;	
+}
+
---
+++ validaLogin.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+// phpinfo();
+// echo "<br/>server: ".$_SERVER['DOCUMENT_ROOT']."<br/>";
+require_once 'configs/default.conf.php'; 
+require_once 'classes/security.class.php';
+
+$login = $_POST['input_user'];
+$password = $_POST['input_pass'];
+// echo "<pre>"; print_r($_POST);echo "</pre>";
+$sc = new security('');
+if ($sc -> authenticateUser ($login,$password)){
+	$_SESSION['User_NA'] = $login;
+	//echo "<pre>"; print_r($_SESSION);echo "</pre>";
+	//die;
+	header('Location: index.php');
+}else{
+	header('Location: login.php?err=1');
+}
+
+
+
+?>
